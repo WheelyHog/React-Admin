a75b9c7d7b5a411cd85bfac2474429427b680012
Wheelyhog <wheelyhog@gmail.com>
2023-11-02 19:00:13 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Home from "./pages/home/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "users",
+      element: <Users />,
+    },
+    {
+      path: "products",
+      element: <Products />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/products/Products.tsx
@@ -0,0 +1,7 @@
+import "./products.scss";
+
+const Products = () => {
+  return <div className="products">Hello</div>;
+};
+
+export default Products;
